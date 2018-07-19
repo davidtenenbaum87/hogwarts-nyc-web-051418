2bba759f4cfb12945832187700a5316ac1c8b027
David Tenenbaum <github email address>
2018-07-19 11:52:27 -0400

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
+import Hog from './Hog'
 import hogs from '../porkers_data';
 
+
 class App extends Component {
+
+
+  loadHogs = () => {
+     return hogs.map(hog => (
+      <Hog
+      name={hog.name}
+      specialty={hog.specialty}
+      greased={hog.greased}
+      weight={hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}
+      medal={hog['highest medal achieved']}
+      />
+    ))
+
+  }
+
   render() {
     return (
       <div className="App">
           < Nav />
-
+          { this.loadHogs() }
       </div>
     )
   }
---
+++ src/components/Hog.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Hog extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: props.name,
+      specialty: props.specialty,
+      greased: props.greased,
+      weight: props.weight,
+      medal: props.medal,
+    }
+  }
+
+  hogImgName = () => {
+    console.log(this.state.name.toLowerCase().split(' ').join('_'))
+    return this.state.name.toLowerCase().split(' ').join('_')
+  }
+
+
+  render() {
+    return (
+      <div className='pigTile'>
+        <img src={".././hog-imgs/" + this.hogImgName() + ".jpg"}/>
+        <p>{this.state.name}</p>
+        <p>{this.state.specialty}</p>
+        <p>{this.state.greased}</p>
+        <p>{this.state.weight}</p>
+        <p>{this.state.medal}</p>
+      </div>
+    )
+  }
+}
+
+export default Hog;
